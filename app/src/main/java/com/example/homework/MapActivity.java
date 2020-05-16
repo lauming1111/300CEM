b588ab357c1c3b8f78762af95a62c89d69639f65
Lau Ming <lauming1111@gmail.com>
2020-05-16 22:48:59 +0800

    add: map

---
+++ app/src/main/java/com/example/homework/MapActivity.java
@@ -0,0 +1,14 @@
+package com.example.homework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapActivity extends AppCompatActivity {
+//const asd =AIzaSyDjWKuDshXdGkPu7fjyH6_fagr6uVIq1OI
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
