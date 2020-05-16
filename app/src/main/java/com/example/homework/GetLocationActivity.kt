c0b56c9fa1ee680e5966a3ab836c8a4635e68cc9
Lau Ming <lauming1111@gmail.com>
2020-05-16 15:35:43 +0800

    add: app icon

--- app/src/main/java/com/example/homework/GetLocationActivity.kt
+++ app/src/main/java/com/example/homework/GetLocationActivity.kt
@@ -1,12 +1,18 @@
 package com.example.homework
 
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.get_location.*
+
 
 class GetLocationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.get_location);
-
+        getLocation.setOnClickListener {
+//            val locationManager =
+//                getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        }
     }
 }
