75276b0eba4df3555aa6a72cd2763aff896b00b2
Lau Ming <lauming1111@gmail.com>
2020-05-16 14:59:57 +0800

    add: location activity

---
+++ app/src/main/java/com/example/homework/GetLocationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.homework
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GetLocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.get_location);
+
+    }
+}
--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.homework
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        setSupportActionBar(toolbar)
+        goToLocation.setOnClickListener {
+            startActivity(Intent(this, GetLocationActivity::class.java))
+        }
     }
 }
